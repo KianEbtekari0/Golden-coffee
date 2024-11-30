66fd2c243795c9327bfe952fc1c20373edc6ad26
unknown <kianebtekari0@gmail.com>
2024-11-30 21:07:36 +0330

    Add assets folder and Header components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Logo from ''
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        {/* Logo & Menu */}
+        <nav>
+            <img src="" alt="" />
+        </nav>
+        
+        {/* Cart & Theme Toggle, Login Link */}
+        <div>
+
+        </div>
+      </header>
+    )
+  }
+}
