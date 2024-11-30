28edf7870b595d8208efbc461dd0f50e32239240
unknown <kianebtekari0@gmail.com>
2024-11-30 20:33:43 +0330

    Setup tailwindcss and add fonts file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="p-5 bg-slate-500">
+      <h1 className='font-Dana'>سلام</h1>
     </div>
   );
 }
