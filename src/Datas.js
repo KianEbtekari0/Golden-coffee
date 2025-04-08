a7211d159ec229cf8c645b8f969a25c5ac0d96df
Kian Ebtekari <kianebtekari0@gmail.com>
2025-04-08 15:50:33 +0330

    Complete the product page

--- src/Datas.js
+++ src/Datas.js
@@ -8,14 +8,14 @@ import img7 from './assets/products/p7.png'
 import img8 from './assets/products/p8.png'
 
 let Datas = [
-    {"id": 1, "img": img1, "description": "قهوه ترک بن مانو به مقدار 100 گرم", "price": 155000, "count": 1},
-    {"id": 2, "img": img2, "description": "قهوه ترک بن مانو به مقدار 50 گرم", "price": 30000, "count": 1},
-    {"id": 3, "img": img3, "description": "قهوه ترک بن مانو به مقدار 80 گرم", "price": 105000, "count": 1},
-    {"id": 4, "img": img4, "description": "قهوه ترک بن مانو به مقدار 150 گرم", "price": 35000, "count": 1},
-    {"id": 5, "img": img5, "description": "قهوه ترک بن مانو به مقدار 250 گرم", "price": 125000, "count": 1},
-    {"id": 6, "img": img6, "description": "قهوه ترک بن مانو به مقدار  300 گرم", "price": 155000, "count": 1},
-    {"id": 7, "img": img7, "description": "قهوه اسپرسو بن مانو مدل پریسکا 500 گرمی", "price": 130000, "count": 1},
-    {"id": 8, "img": img8, "description": "قهوه ترک بن مانو به مقدار 350 گرم", "price": 250000, "count": 1}
+    {"id": 1, "img": img1, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار 100 گرم", "price": 155000, available: true, "count": 1},
+    {"id": 2, "img": img2, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار 50 گرم", "price": 30000, available: false, "count": 1},
+    {"id": 3, "img": img3, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار 80 گرم", "price": 105000, available: true, "count": 1},
+    {"id": 4, "img": img4, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار 150 گرم", "price": 35000, available: false, "count": 1},
+    {"id": 5, "img": img5, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار 250 گرم", "price": 125000, available: true, "count": 1},
+    {"id": 6, "img": img6, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار  300 گرم", "price": 155000, available: true, "count": 1},
+    {"id": 7, "img": img7, name: 'اسپرسو بن مانو', "description": "قهوه اسپرسو بن مانو مدل پریسکا 500 گرمی", "price": 130000, available: false, "count": 1},
+    {"id": 8, "img": img8, name: 'قهوه ترک بن مانو', "description": "قهوه ترک بن مانو به مقدار 350 گرم", "price": 250000, available: true, "count": 1}
 ]
 
 export default Datas
