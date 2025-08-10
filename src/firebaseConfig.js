dff1d82575fabc0a2114e4686ff0ada040166b41
Kian Ebtekari <kianebtekari0@gmail.com>
2025-08-10 22:30:35 +0330

    [fix]: make secrets files

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRsJiXP-XCC8YMbOgff9CpMnq7v8OXBrA",
-  authDomain: "gold-coffee-d2f13.firebaseapp.com",
-  databaseURL: "https://gold-coffee-d2f13-default-rtdb.firebaseio.com",
-  projectId: "gold-coffee-d2f13",
-  storageBucket: "gold-coffee-d2f13.firebasestorage.app",
-  messagingSenderId: "1051365954304",
-  appId: "1:1051365954304:web:a0d3e9f8e81b05f0ba19e5",
-  measurementId: "G-K3RXD1TP56"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
